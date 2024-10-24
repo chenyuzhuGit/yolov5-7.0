a3552e2b3fd0b5c1a1392a8e3b7fde6fc1e589c3
陈玉竹 <chenyuzhu@dcm360.cn>
2024-10-24 15:49:59 +0800

    更新注释文件

---
+++ other/todo.py
@@ -0,0 +1,7 @@
+"""
+注释来源：https://blog.csdn.net/qq_51511878/article/details/130004796
+Run YOLOv5 detection inference on images, videos, directories, globs, YouTube, webcam, streams, etc.
+TODO
+    1.迁移截图功能
+    2.迁移键鼠功能
+"""
